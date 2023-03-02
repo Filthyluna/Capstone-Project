d2bfeba63135bd83c498d86bf317e23a18f8897d
unknown <melanievasquez@nypl.org>
2023-03-02 11:42:23 -0500

    Added randomization on reload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import axios from "axios";
 import React from "react";
-function App() {
-  
+
+const url = "https://gutendex.com/books/";
+
+export default function Book() {
+  let id = Math.floor(Math.random() * 12000)
+  const [book, setBook] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(url + id)
+      .then((res) => {
+        console.log(res.data);
+        setBook(res.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
+  if (!book) return null
+
+  return (
+    <div className="book-info">
+      <h1>{book.title}</h1>
+      <h2>{book.authors[0].name.split(',').reverse().join(' ')}</h2>
+      <p>{book.formats[3]}</p>
+    </div>
+  );
 }
 
-export default App;

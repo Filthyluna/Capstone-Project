2500fd53360ba453856a1a337f57673254050138
Mel <56780129+Filthyluna@users.noreply.github.com>
2023-05-05 18:27:39 -0400

    Update Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,7 +7,7 @@ return(props.trigger) ? (
     <div className ="popup">
     	<div className="popup-inner">
     	  <button className="close-btn" onClick={() => props.setTrigger(false)}
-   	 ><img src="https://cdn-icons-png.flaticon.com/24/4421/4421536.png"/></button>
+   	 ><img src="https://cdn-icons-png.flaticon.com/24/4421/4421536.png" alt="x"/></button>
       {props.children}
    	 </div>
     </div>

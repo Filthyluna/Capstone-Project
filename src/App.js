7636708ec80d5ca1cd4a9f00e19a09b93b6aaed7
urooj <Urooj.Ansari364@myhunter.cuny.edu>
2023-05-05 16:12:50 -0400

    Popup part 1

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import HomePage from './components/HomePage';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Popup from './components/Popup';
 import BookDetails from './components/BookDetails';
-
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+
         <Route path="/" element={<HomePage />} />
         <Route path="/BookList" element={<BookList />} /> 
         <Route path="/book/:id" element={<BookDetails/>} />
       </Routes>
+
       <Footer/>
     </div>
   );
---
+++ src/components/Popup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Popup.css'  
+import './BookList'
+function Popup(props){
+return(props.trigger) ? (
+    <div className ="popup">
+    	<div className="popup-inner">
+    	  <button className="close-btn" onClick={() => props.setTrigger(false)}
+   	 ><img src="https://cdn-icons-png.flaticon.com/24/4421/4421536.png"/></button>
+    	  {props.children} 
+   	 </div>
+    </div>
+  ) : ""; 
+}
+
+export default Popup
